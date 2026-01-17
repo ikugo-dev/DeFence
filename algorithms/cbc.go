526c25a058fe24eef13210699e5332968621c7d9
Aleksandar Ristic <aleksandarristic503@gmail.com>
2026-01-17 03:43:04 +0100

    added cbc for xxtea and modified xxtea

--- algorithms/cbc.go
+++ algorithms/cbc.go
@@ -1,8 +1,95 @@
 package algorithms
 
-func EncryptCBC(data []byte, key []byte) []byte {
-	return data
+import (
+	"bytes"
+	"log"
+)
+
+const BlockSize = 16
+
+func EncryptCBC(data []byte, key []byte, iv []byte) []byte {
+	if len(iv) != BlockSize {
+		log.Fatal("invalid IV size")
+	}
+	if len(key) != KeySize {
+		log.Fatal("invalid key size")
+	}
+
+	data = pkcs7Pad(data)
+
+	out := make([]byte, 0, len(iv)+len(data))
+	out = append(out, iv...)
+
+	prev := iv
+
+	for i := 0; i < len(data); i += BlockSize {
+		block := data[i : i+BlockSize]
+		xored := xorBlocks(block, prev)
+		enc := EncryptXXTEA(xored, key)
+		out = append(out, enc...)
+		prev = enc
+	}
+
+	return out
 }
 func DecryptCBC(data []byte, key []byte) []byte {
-	return data
+	if len(data) < BlockSize*2 {
+		log.Fatal("ciphertext too short")
+	}
+	if len(key) != KeySize {
+		log.Fatal("invalid key size")
+	}
+
+	iv := data[:BlockSize]
+	data = data[BlockSize:]
+
+	prev := iv
+	out := make([]byte, 0, len(data))
+
+	for i := 0; i < len(data); i += BlockSize {
+		block := data[i : i+BlockSize]
+		dec := DecryptXXTEA(block, key)
+		plain := xorBlocks(dec, prev)
+		out = append(out, plain...)
+		prev = block
+	}
+
+	out = pkcs7Unpad(out)
+	if out == nil {
+		log.Fatal("invalid padding")
+	}
+	return out
+}
+
+func xorBlocks(a, b []byte) []byte {
+	out := make([]byte, BlockSize)
+	for i := range BlockSize {
+		out[i] = a[i] ^ b[i]
+	}
+	return out
+}
+
+func pkcs7Pad(data []byte) []byte {
+	padLen := BlockSize - (len(data) % BlockSize)
+	if padLen == 0 {
+		padLen = BlockSize
+	}
+	pad := bytes.Repeat([]byte{byte(padLen)}, padLen)
+	return append(data, pad...)
+}
+
+func pkcs7Unpad(data []byte) []byte {
+	if len(data) == 0 || len(data)%BlockSize != 0 {
+		return nil
+	}
+	padLen := int(data[len(data)-1])
+	if padLen == 0 || padLen > BlockSize {
+		return nil
+	}
+	for i := range padLen {
+		if data[len(data)-1-i] != byte(padLen) {
+			return nil
+		}
+	}
+	return data[:len(data)-padLen]
 }
--- algorithms/xxtea.go
+++ algorithms/xxtea.go
@@ -3,35 +3,43 @@ package algorithms
 import (
 	"bytes"
 	"encoding/binary"
+	"log"
 )
 
 const DELTA = 0x9e3779b9
+const MinDataSize = 16
+const KeySize = 16
 
 func MX(y, z, sum, p, e uint32, key []uint32) uint32 {
 	return (((z>>5 ^ y<<2) + (y>>3 ^ z<<4)) ^ ((sum ^ y) + (key[(p&3)^e] ^ z)))
 }
 
 func EncryptXXTEA(data []byte, key []byte) []byte {
-	//TODO: proveri da li je len(key) == 4
+	if len(data) < MinDataSize {
+		log.Fatal("invalid data size")
+	}
+	if len(key) != KeySize {
+		log.Fatal("invalid key size")
+	}
 	v := bytesToUint32s(data)
 	n := len(v)
 	k := bytesToUint32s(key)
+
 	var y, z, sum uint32
-	var rounds, e uint32
-	rounds = uint32(6 + 52/n)
+	rounds := uint32(6 + 52/n)
 	sum = 0
 	z = v[n-1]
+
 	for rounds > 0 {
 		sum += DELTA
-		e = (sum >> 2) & 3
-		p := 0
-		for ; p < n-1; p++ {
+		e := (sum >> 2) & 3
+		for p := 0; p < n-1; p++ {
 			y = v[p+1]
 			v[p] += MX(y, z, sum, uint32(p), e, k)
 			z = v[p]
 		}
 		y = v[0]
-		v[n-1] += MX(y, z, sum, uint32(p), e, k)
+		v[n-1] += MX(y, z, sum, uint32(n-1), e, k)
 		z = v[n-1]
 		rounds--
 	}
@@ -39,28 +47,31 @@ func EncryptXXTEA(data []byte, key []byte) []byte {
 }
 
 func DecryptXXTEA(data []byte, key []byte) []byte {
+	if len(data) < MinDataSize {
+		log.Fatal("invalid data size")
+	}
+	if len(key) != KeySize {
+		log.Fatal("invalid key size")
+	}
 	v := bytesToUint32s(data)
 	n := len(v)
 	k := bytesToUint32s(key)
 
 	var y, z, sum uint32
-	var rounds, e uint32
-	rounds = uint32(6 + 52/n)
+	rounds := uint32(6 + 52/n)
 	sum = rounds * DELTA
 	y = v[0]
 	for rounds > 0 {
-		e = (sum >> 2) & 3
-		p := n - 1
-		for ; p > 0; p-- {
+		e := (sum >> 2) & 3
+		for p := n - 1; p > 0; p-- {
 			z = v[p-1]
 			v[p] -= MX(y, z, sum, uint32(p), e, k)
 			y = v[p]
 		}
 		z = v[n-1]
-		v[0] -= MX(y, z, sum, uint32(p), e, k)
+		v[0] -= MX(y, z, sum, 0, e, k)
 		y = v[0]
 		sum -= DELTA
-
 		rounds--
 	}
 	return uint32ToBytes(v)
